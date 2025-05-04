1a6bb8e64d9dfc2e2e005599f82f1db747c3b418
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-04 17:52:49 +0530

    Update Chocolate Distribution Problem.py

--- 1) Array /GFG - Chocolate Distribution Problem /Chocolate Distribution Problem.py
+++ 1) Array /GFG - Chocolate Distribution Problem /Chocolate Distribution Problem.py
@@ -1,1 +1,9 @@
 def findMinDiff(self, arr,M):
+  if m == 0 or len(arr) < m: return 0
+  arr.sort()
+  min_diff = float('inf')
+  
+  for i in range(len(arr) - m + 1):
+    diff = arr[i + m - 1] - arr[i]
+    min_diff = min(min_diff, diff)
+  return min_diff
