a399be0bba72cb4f1468ebb8f8e72e709b9dc8de
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-06 23:16:50 +0530

    Update 11) Container With Most Water.py

--- 1) Array /11) Container With Most Water /11) Container With Most Water.py
+++ 1) Array /11) Container With Most Water /11) Container With Most Water.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        for i in range(len(height)):
+            for j in range(1, len(height)):
+                curr_area = min(height[i],height[j]) * (j-i)
+                max_area = max(max_area, curr_area)
+        return max_area
